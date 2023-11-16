dca54e3076e7ff103b3be790dccc047cc314009d
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-11-16 10:41:21 -0800

    Implemented code to enable 'New Player' button on CongratsFragment to navigate to WelcomeFragment

--- app/src/main/java/com/example/guessinggame/CongratsFragment.kt
+++ app/src/main/java/com/example/guessinggame/CongratsFragment.kt
@@ -5,12 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+
 class CongratsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_congrats, container, false)
+        val view = inflater.inflate(R.layout.fragment_congrats, container, false)
+        val newPlayer = view.findViewById<Button>(R.id.newPlayerBtn)
+
+        newPlayer.setOnClickListener{
+            view.findNavController().navigate(R.id.welcomeFragment)
+        }
+
+        return view
     }
 }
