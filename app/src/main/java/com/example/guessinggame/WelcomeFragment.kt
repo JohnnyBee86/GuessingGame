04970b6275a7aa3550043d4792e14e3d3e2fb184
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-11-16 13:16:48 -0800

    Implemented code to ensure a player name is entered on the WelcomeFragment before navigating to the GameFragment.  Implemented a toast to display instructions if a name is not present when the 'Play' button is clicked

--- app/src/main/java/com/example/guessinggame/WelcomeFragment.kt
+++ app/src/main/java/com/example/guessinggame/WelcomeFragment.kt
@@ -7,9 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
-import androidx.navigation.findNavController
+import android.widget.Toast
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 
 class WelcomeFragment : Fragment() {
+
+    private lateinit var nameText: EditText
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,16 +24,33 @@ class WelcomeFragment : Fragment() {
 
         // Declare 'Play' button and variable to hold player name
         val playButton = view.findViewById<Button>(R.id.playBtn)
-        val nameText = view.findViewById<EditText>(R.id.playerNameText)
+        nameText = view.findViewById<EditText>(R.id.playerNameText)
 
         // Assign function of 'Play' button
         // Get player name and pass it to next GameFragment
         playButton.setOnClickListener{
+            // Get name from EditText
             val playerName = nameText.text.toString()
+            // Attach name to NavDirections
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToGameFragment(playerName)
-            view.findNavController().navigate(action)
+            // If valid execute action
+            checkName(action)
         }
 
         return view
+    }
+
+    // Function to check for a 'name'
+    private fun checkName(action: NavDirections) {
+        // If a name is not present?
+        if (nameText.text.isEmpty()) {
+            // If a name is not present show a toast with 'instructions'
+            Toast.makeText(activity, getString(R.string.main_name_required), Toast.LENGTH_SHORT).show()
+        }
+        // If a name is present
+        else {
+            // Engage!
+            findNavController().navigate(action)
+        }
     }
 }
